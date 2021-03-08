7f1d908e8c7b3999cb6f9990df27ac9a824743ba
daniele-bit <daniele.fas95@gmail.com>
2021-03-08 18:19:41 +0100

    Risolto Inizializzare Spring.io #1

---
+++ garage-esercitazione-server/garage-esercitazione/src/main/java/it/sirfin/garageesercitazione/GarageEsercitazioneApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.garageesercitazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageEsercitazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageEsercitazioneApplication.class, args);
+	}
+
+}
